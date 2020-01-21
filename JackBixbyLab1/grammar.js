13a19b15174879f6f290cd047202ba9ba94b0163
Jack Bixby <5643io5643io@gmail.com>
2020-01-21 09:08:44 -0500

    Finished Lab 1

--- JackBixbyLab1/grammar.js
+++ JackBixbyLab1/grammar.js
@@ -4,9 +4,17 @@ var Grammar = /** @class */ (function () {
     function Grammar(grammar) {
         this.set = new Set();
         var input = grammar.split("\n");
-        for (var st in input) {
-            var input2 = st.split(" -> ");
-            console.log("Left: " + input2[0] + " Right: " + input2[0]);
+        for (var i = 0; i < input.length - 1; i++) {
+            if (input[i].split(" -> ").length != 2) {
+                throw new Error("Inncorrect length of " + input[i]);
+            }
+            var input2 = input[i].split(" -> ");
+            console.log("Left: " + input2[0] + " Right: " + input2[1]);
+            if (this.set.has(input2[0])) {
+                throw new Error("Grammar contains multiple variables " + input2[0]);
+            }
+            this.set.add(input2[0]);
+            var rex = new RegExp(input2[1], "gy");
         }
     }
     return Grammar;
--- JackBixbyLab1/grammar.ts
+++ JackBixbyLab1/grammar.ts
@@ -3,9 +3,17 @@ export class Grammar {
     constructor(grammar: string) {
         this.set = new Set();
         let input = grammar.split("\n");
-        for (let st in input) {
-            let input2 = st.split(" -> ");
-            console.log("Left: " + input2[0] + " Right: " + input2[0]);
+        for (let i = 0; i < input.length - 1; i++) {
+            if (input[i].split(" -> ").length != 2) {
+                throw new Error("Inncorrect length of " + input[i]);
+            }
+            let input2 = input[i].split(" -> ");
+            console.log("Left: " + input2[0] + " Right: " + input2[1]);
+            if (this.set.has(input2[0])) {
+                throw new Error("Grammar contains multiple variables " + input2[0]);
+            }
+            this.set.add(input2[0]);
+            let rex = new RegExp(input2[1], "gy");
         }
     }
 }
