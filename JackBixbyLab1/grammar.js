4bf33884c1e7844bcbeb8e2c3e6f4c6e6c46e59d
Jack Bixby <5643io5643io@gmail.com>
2020-02-27 10:07:52 -0500

    finished this bby

--- JackBixbyLab1/grammar.js
+++ JackBixbyLab1/grammar.js
@@ -1,19 +1,7 @@
 "use strict";
-var __values = (this && this.__values) || function(o) {
-    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
-    if (m) return m.call(o);
-    if (o && typeof o.length === "number") return {
-        next: function () {
-            if (o && i >= o.length) o = void 0;
-            return { value: o && o[i++], done: !o };
-        }
-    };
-    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-};
 exports.__esModule = true;
 var Grammar = /** @class */ (function () {
     function Grammar(grammar) {
-        var e_1, _a;
         this.set = new Set();
         this.terminals = new Array();
         this.nonterminals = new Map();
@@ -27,7 +15,7 @@ var Grammar = /** @class */ (function () {
                 throw new Error("First:Inncorrect length of " + input[i]);
             }
             var input2 = input[i].split(" -> ");
-            console.log("Left: " + input2[0] + " Right: " + input2[1]);
+            //console.log("Left: " + input2[0] + " Right: " + input2[1]);
             if (this.set.has(input2[0])) {
                 throw new Error("Grammar contains multiple variables " + input2[0]);
             }
@@ -39,62 +27,114 @@ var Grammar = /** @class */ (function () {
         this.terminals.push(["WHITESPACE", RegExp(/\s/gy)]);
         var back_i = i;
         for (i++; i < input.length - 1; i++) {
-            if (input[i].split(" -> ").length !== 2) {
-                throw new Error("Second:Inncorrect length of " + input[i] + "!");
+            var input2 = input[i].split("->");
+            input2.forEach(function (val) {
+                val.trim();
+            });
+            if (input2.length !== 2) {
+                continue;
+                //throw new Error("Second:Inncorrect length of " + input[i] + "!");
             }
-            var input2 = input[i].split(" -> ");
-            console.log("Left: " + input2[0] + " Right: " + input2[1]);
+            //console.log("Left: " + input2[0] + " Right: " + input2[1]);
             if (this.set.has(input2[0])) {
                 //throw new Error("Grammar contains multiple variables " + input2[0]);
             }
-            this.set.add(input2[0]);
+            this.set.add(input2[0].trim());
         }
         var final_i = back_i;
         for (back_i++; back_i < input.length - 1; back_i++) {
-            var input2 = input[back_i].split(" -> ");
-            var input3 = input2[1].split(" ");
-            console.log("Input3: " + input3);
-            for (var j = 0; j < input3.length; j++) {
-                if (!this.set.has(input3[j]) && input3[j] != "|" && input3[j] != "") {
+            var input2 = input[back_i].split("->");
+            input2.forEach(function (val) {
+                val.trim();
+            });
+            if (input2.length !== 2) {
+                continue;
+            }
+            /*let input3 = input2[1].split("|");
+            input3.forEach((val) => {
+                val.trim();
+            });
+            if (input3.length !== 2) {
+                continue;
+            }
+            //console.log("Input3: " + input3);
+            for (let j = 0; j < input3.length; j++) {
+                if (!this.set.has(input3[j]) && input3[j] != "|" && input3[j] != "" && input3[j] != "lambda") {
                     throw new Error("Undefined Sign " + input3[j]);
                 }
-            }
+            }*/
+            var input3 = input2[1].split("|");
+            input3.forEach(function (val) {
+                val.trim();
+            });
+            /*if (input3.length !== 2) {
+                continue;
+            }*/
+            var input4 = new Array();
+            var input5 = void 0;
             for (var j = 0; j < input3.length; j++) {
-                if (input3[j] != "|" && input3[j] != "") {
-                    if (this.nonterminals.has(input2[0])) {
-                        this.nonterminals.get(input2[0]).push(input3[j]);
-                    }
-                    else {
-                        this.nonterminals.set(input2[0], [input3[j]]);
-                    }
-                }
+                input5 = input3[j].split(" ");
+                input5.forEach(function (val) {
+                    val.trim();
+                });
+                input4.push(input5.filter(function (element, index) {
+                    return element !== "";
+                }));
+            }
+            if (this.nonterminals.has(input2[0].trim())) {
+                this.nonterminals.get(input2[0].trim()).concat(input4);
+            }
+            else {
+                this.nonterminals.set(input2[0].trim(), input4);
             }
         }
-        console.log(this.nonterminals);
-        console.log(this.terminals);
-        var visited = new Set();
-        var first_string = this.nonterminals.keys();
+        //console.log(this.nonterminals);
+        //console.log(this.terminals);
+        /*let visited = new Set<string>();
+        const first_string = this.nonterminals.keys();
         dfs(first_string.next().value, this.nonterminals, visited, this.terminals);
         console.log("Visited Size: " + visited.size);
         if (this.nonterminals.size == 0) {
             return;
         }
-        try {
-            for (var _b = __values(this.set.keys()), _c = _b.next(); !_c.done; _c = _b.next()) {
-                var k = _c.value;
-                if (!visited.has(k) && k !== "WHITESPACE") {
-                    throw new Error("Useless Production Found " + k);
-                }
+        for(let k of this.set.keys() ) {
+            if (!visited.has(k) && k !== "WHITESPACE") {
+                throw new Error("Useless Production Found " + k);
             }
-        }
-        catch (e_1_1) { e_1 = { error: e_1_1 }; }
-        finally {
-            try {
-                if (_c && !_c.done && (_a = _b["return"])) _a.call(_b);
-            }
-            finally { if (e_1) throw e_1.error; }
-        }
+        }*/
     }
+    Grammar.prototype.getNullable = function () {
+        var nullable = new Set();
+        var _loop_1 = function () {
+            var flag = true;
+            this_1.nonterminals.forEach(function (v, k) {
+                v.forEach(function (P) {
+                    if (P.length == 1 && P[0] == "lambda") {
+                        P = new Array();
+                    }
+                    if (!nullable.has(k)) {
+                        var horse = P.every(function (x) {
+                            return nullable.has(x);
+                        });
+                        if (horse) {
+                            nullable.add(k);
+                            flag = false;
+                        }
+                    }
+                });
+            });
+            if (flag) {
+                return "break";
+            }
+        };
+        var this_1 = this;
+        while (true) {
+            var state_1 = _loop_1();
+            if (state_1 === "break")
+                break;
+        }
+        return nullable;
+    };
     return Grammar;
 }());
 exports.Grammar = Grammar;
--- JackBixbyLab1/grammar.ts
+++ JackBixbyLab1/grammar.ts
@@ -1,11 +1,11 @@
 export class Grammar {
     set: Set<string>;
     terminals: Array<[string, RegExp]>;
-    nonterminals: Map<string, string[]>;
+    nonterminals: Map<string, string[][]>;
     constructor(grammar: string) {
         this.set = new Set();
         this.terminals = new Array<[string, RegExp]>();
-        this.nonterminals = new Map<string, string[]>();
+        this.nonterminals = new Map<string, string[][]>();
         let input = grammar.split("\n");
         let i = 0;
         for (; i < input.length - 1; i++) {
@@ -16,7 +16,7 @@ export class Grammar {
                 throw new Error("First:Inncorrect length of " + input[i]);
             }
             let input2 = input[i].split(" -> ");
-            console.log("Left: " + input2[0] + " Right: " + input2[1]);
+            //console.log("Left: " + input2[0] + " Right: " + input2[1]);
             if (this.set.has(input2[0])) {
                 throw new Error("Grammar contains multiple variables " + input2[0]);
             }
@@ -28,39 +28,71 @@ export class Grammar {
         this.terminals.push(["WHITESPACE", RegExp(/\s/gy)]);
         let back_i = i;
         for (i++; i < input.length - 1; i++) {
-            if (input[i].split(" -> ").length !== 2) {
-                throw new Error("Second:Inncorrect length of " + input[i] + "!");
+            let input2 = input[i].split("->");
+            input2.forEach((val) => {
+                val.trim();
+            });
+            if (input2.length !== 2) {
+                continue;
+                //throw new Error("Second:Inncorrect length of " + input[i] + "!");
             }
-            let input2 = input[i].split(" -> ");
-            console.log("Left: " + input2[0] + " Right: " + input2[1]);
+            
+            //console.log("Left: " + input2[0] + " Right: " + input2[1]);
             if (this.set.has(input2[0])) {
                 //throw new Error("Grammar contains multiple variables " + input2[0]);
             }
-            this.set.add(input2[0]);
+            this.set.add(input2[0].trim());
         }
         let final_i = back_i;
         for (back_i++; back_i < input.length - 1; back_i++) {
-            let input2 = input[back_i].split(" -> ");
-            let input3 = input2[1].split(" ");
-            console.log("Input3: " + input3);
+            let input2 = input[back_i].split("->");
+            input2.forEach((val) => {
+                val.trim();
+            });
+            if (input2.length !== 2) {
+                continue;
+            }
+            /*let input3 = input2[1].split("|");
+            input3.forEach((val) => {
+                val.trim();
+            });
+            if (input3.length !== 2) {
+                continue;
+            }
+            //console.log("Input3: " + input3);
             for (let j = 0; j < input3.length; j++) {
-                if (!this.set.has(input3[j]) && input3[j] != "|" && input3[j] != "") {
+                if (!this.set.has(input3[j]) && input3[j] != "|" && input3[j] != "" && input3[j] != "lambda") {
                     throw new Error("Undefined Sign " + input3[j]);
                 }
-            }
+            }*/
+            let input3 = input2[1].split("|");
+            input3.forEach((val) => {
+                val.trim();
+            });
+            /*if (input3.length !== 2) {
+                continue;
+            }*/
+            let input4 = new Array<Array<string>>();
+            let input5;
             for (let j = 0; j < input3.length; j++) {
-                if (input3[j] != "|" && input3[j] != "") {
-                    if (this.nonterminals.has(input2[0])) {
-                        this.nonterminals.get(input2[0]).push(input3[j]);
-                    } else {
-                        this.nonterminals.set(input2[0], [input3[j]]);
-                    }
-                }
+                input5 = input3[j].split(" ");
+                input5.forEach((val) => {
+                    val.trim();
+                });
+                input4.push(input5.filter((element, index) => {
+                    return element !== "";
+                }));
+
+            }
+            if (this.nonterminals.has(input2[0].trim())) {
+                this.nonterminals.get(input2[0].trim()).concat(input4);
+            } else {
+                this.nonterminals.set(input2[0].trim(), input4);
             }
         }
-        console.log(this.nonterminals);
-        console.log(this.terminals);
-        let visited = new Set<string>();
+        //console.log(this.nonterminals);
+        //console.log(this.terminals);
+        /*let visited = new Set<string>();
         const first_string = this.nonterminals.keys();
         dfs(first_string.next().value, this.nonterminals, visited, this.terminals);
         console.log("Visited Size: " + visited.size);
@@ -71,7 +103,34 @@ export class Grammar {
             if (!visited.has(k) && k !== "WHITESPACE") {
                 throw new Error("Useless Production Found " + k);
             }
+        }*/
+    }
+
+    getNullable() {
+        let nullable = new Set<string>();
+        while (true) {
+            let flag = true;
+            this.nonterminals.forEach((v, k) => {
+                v.forEach((P: string[]) => {
+                    if (P.length == 1 && P[0] == "lambda") {
+                        P = new Array<string>();
+                    }
+                    if (!nullable.has(k)) {
+                        let horse = P.every((x: string) => {
+                            return nullable.has(x);
+                        });
+                        if (horse) {
+                            nullable.add(k);
+                            flag = false;
+                        }
+                    }
+                });
+            });
+            if (flag) {
+                break;
+            }
         }
+        return nullable;
     }
 }
 
